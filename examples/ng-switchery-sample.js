7c19cf6d6fcfb4c9e5680a88babe4327a2134d08
bornkiller <hjj491229492@hotmail.com>
2014-03-02 15:45:52 +0800

    modify the demo which achieve data-binding function

--- examples/ng-switchery-sample.js
+++ examples/ng-switchery-sample.js
@@ -1,3 +1,7 @@
 'use strict';
 
-var sampleApp = angular.module('NgSwitcherySample', ['NgSwitchery']);
+var sampleApp = angular.module('NgSwitcherySample', ['NgSwitchery'])
+   .controller('NgSwitcheryCtrl', ['$scope', function($scope){
+   	    $scope.first = false;
+   	    $scope.second = true;
+   }])
