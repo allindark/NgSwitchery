02e0212448671856782bae389336dac3eb3831fd
bornkiller <hjj491229492@hotmail.com>
2015-01-15 21:22:36 +0800

    update example.

--- examples/ng-switchery-sample.js
+++ examples/ng-switchery-sample.js
@@ -1,16 +1,8 @@
 angular.module('NgSwitcherySample', ['NgSwitchery'])
-	.controller('NgSwitcheryCtrl', ['$scope', '$interval', '$log',
-		function($scope, $interval, $log) {
+	.controller('NgSwitcheryCtrl', ['$scope',
+		function($scope) {
 			$scope.active = true;
 			$scope.program = false;
 			$scope.switcheryDisabled = true;
-			$interval(function() {
-				$log.info('active value' + $scope.active.toString());
-				$log.info('program value' + $scope.active.toString());
-				$log.info('manualDisabled value' + $scope.manualDisabled.toString());
-				$scope.active = $scope.active ? false : true;
-				$scope.program = $scope.program ? false : true;
-				$scope.switcheryDisabled = $scope.switcheryDisabled ? false : true;
-			}, 1000, 3);
 		}
 	]);
